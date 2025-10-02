b31a94f5fe46b139ff042b229aa34cfc816d99ba
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-01 22:26:33 -0500

    conditionally_implement_methods

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,47 @@
+use std:: fmt::Display;
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+        largest = item;
+    }
+    }
+    largest
+}
+
 fn main() {
-    pub trait Summary {
-        fn summarize_author(&self) -> String;
-
-        fn summarize(&self) -> String {
-            format!("(Read more from {}...)", self.summarize_author())
-        }
-    }
-
-    pub struct NewsArticle {
-        pub headline: String,
-        pub location: String,
-        pub author: String,
-        pub content: String,
-    }
-
-    impl Summary for NewsArticle {
-        fn summarize_author(&self) -> String {
-            self.author.clone() // Return the author's name
-        }
-
-        fn summarize(&self) -> String {
-            format!("{}, by {} ({})", self.headline, self.author, self.location)
-        }
-    }
-
-    pub struct Tweet {
-        pub username: String,
-        pub content: String,
-        pub reply: bool,
-        pub retweet: bool,
-    }
-
-    impl Summary for Tweet {
-        fn summarize_author(&self) -> String {
-            format!("@{}", self.username)
-        }
-    }
-
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
-    println!("1 new tweet: {}", tweet.summarize());    
-
+    let number_list = vec![34, 50, 25, 100, 65];
     
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
-    fn returns_summarizable() -> impl Summary {
-        Tweet {
-            username: String::from("horse_ebooks"),
-            content: String::from("of course, as you probably already know, people"),
-            reply: false,
-            retweet: false,
+    struct Pair<T> {
+        x: T,
+        y: T,
+    }
+
+    impl<T> Pair<T> {
+        fn new(x:T, y:T) -> Self {
+            Self {
+                x,
+                y,
+            }
         }
     }
-    println!("2 new tweet: {}", returns_summarizable().summarize());
+
+    impl<T: Display + PartialOrd> Pair<T> {
+        fn cmp_display(&self) {
+            if self.x >= self.y {
+                println!("The largest member is x = {}", self.x);
+            } else {
+                println!("The largest member is y = {}", self.y);
+            }
+        }
+    }
 }
