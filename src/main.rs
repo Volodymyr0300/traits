f1e08c1af6cb3ffc6330816acfaf883f8ad67412
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-03 16:26:11 -0500

    lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,17 @@
-use std:: fmt::Display;
-
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
-    for &item in list.iter() {
-        if item > largest {
-        largest = item;
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
-    }
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-    
-    let char_list = vec!['y', 'm', 'a', 'q'];
-    
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
-
-    struct Pair<T> {
-        x: T,
-        y: T,
-    }
-
-    impl<T> Pair<T> {
-        fn new(x:T, y:T) -> Self {
-            Self {
-                x,
-                y,
-            }
-        }
-    }
-
-    impl<T: Display + PartialOrd> Pair<T> {
-        fn cmp_display(&self) {
-            if self.x >= self.y {
-                println!("The largest member is x = {}", self.x);
-            } else {
-                println!("The largest member is y = {}", self.y);
-            }
-        }
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
     }
 }
