e210cc280ae8efca461d39d4198384658a620a3e
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-04 22:24:49 -0500

    generic types parameters_trait bounds_and lifetimes together

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() { x } else { y }    
+}
+
 fn main() {
-    let s: &'static str = "I have a static lifetime.";
-    println!("{}", s);
+    println!("{}", longest_with_an_announcement("short", "longer string", "Here is an announcement"));
 }
